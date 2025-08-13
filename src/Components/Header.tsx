a65a20b1dee8385b6b3d21f51b02d3e9abd4b2c6
edch <soderblomed@gmail.com>
2025-08-13 13:01:08 +0200

    add nav in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,9 @@
-import { DigiButton, DigiHeader } from "@digi/arbetsformedlingen-react";
+import {
+  DigiButton,
+  DigiHeader,
+  DigiHeaderNavigation,
+  DigiHeaderNavigationItem,
+} from "@digi/arbetsformedlingen-react";
 
 export default function Header() {
   return (
@@ -26,6 +31,21 @@ export default function Header() {
           fill="#FF4D00"
         ></path>
       </svg>
+      <a slot="header-logo" aria-label="Designsystemets startsida" href="/"></a>
+      <div slot="header-navigation">
+        <DigiHeaderNavigation
+          afCloseButtonText="Stäng"
+          afCloseButtonAriaLabel="Stäng meny"
+          afNavAriaLabel="Huvudmeny"
+        >
+          <DigiHeaderNavigationItem afCurrentPage={true}>
+            <a href="about">About</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem>
+            <a href="contact">Contact</a>
+          </DigiHeaderNavigationItem>
+        </DigiHeaderNavigation>
+      </div>
     </DigiHeader>
   );
 }
