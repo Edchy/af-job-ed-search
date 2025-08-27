d5299869e4dcad9414fd0ab71b107f3fd28d9071
Feliziaboustedt <felizia.boustedt@hotmail.com>
2025-08-27 09:56:19 +0200

    added content to homepage, first draft

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,3 +1,22 @@
+import "./HomePage.css";
+import { Link } from "react-router";
+
 export default function HomePage() {
-  return <p />;
+  return (
+    <>
+      <div className="bg-homepage">
+        <section className="section-bg-color">
+          <h3>Sök efter ditt nästa karriärssteg:</h3>
+          <div className="btn-group">
+            <button className="btn-homepage">
+              <Link to="/utbildningar">Hitta Utbildning</Link>
+            </button>
+            <button className="btn-homepage">
+              <Link to="/utbildningar">Hitta Jobb</Link>
+            </button>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
